6852531943c0afa4fac703524f9513571c7ede60
Wilberg <wilberg@netrune.com>
2025-02-22 18:02:58 +0100

    feat: openapi! macro for rendering templates against an OpenAPI schema compile-time (#2)

--- crates/libs/openapi_schema/src/lib.rs
+++ crates/libs/openapi_schema/src/lib.rs
@@ -1,1 +1,6 @@
 pub type OpenApiSchema = openapiv3::OpenAPI;
+
+pub fn load(path: &str) -> OpenApiSchema {
+    let file = std::fs::read_to_string(path).unwrap();
+    serde_yaml::from_str(&file).unwrap()
+}
