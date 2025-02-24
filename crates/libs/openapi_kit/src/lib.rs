3becd100960c98b1746616d86493b2c7d8b84bea
wilberg <wilberg@netrune.com>
2025-02-24 23:09:20 +0100

    fix(release): add version and data required by crates.io

--- crates/libs/openapi_kit/src/lib.rs
+++ crates/libs/openapi_kit/src/lib.rs
@@ -1,5 +1,7 @@
-pub use openapi_macros as macros;
+pub use openapi_kit_macros as macros;
+pub use openapi_kit_schema as schema;
 
 pub mod prelude {
     pub use crate::macros::*;
+    pub use crate::schema::*;
 }
