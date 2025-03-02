0d24174b6b5c18b67021f6a7eeef162286d80ab6
wilberg <wilberg@netrune.com>
2025-03-02 19:26:31 +0100

    feat: rewrite from_file! macro, and introduce from_template! macro

--- crates/libs/openapi_kit_macros/src/lib.rs
+++ crates/libs/openapi_kit_macros/src/lib.rs
@@ -1,10 +1,20 @@
-mod generate_from_file;
+mod generate;
 
 use proc_macro::TokenStream;
+use syn::{LitStr, parse_macro_input};
 
-/// Generate code from a template file
+/// Generate code from a file
 ///
 #[proc_macro]
-pub fn from_file(input: TokenStream) -> TokenStream {
-    generate_from_file::parse(input)
+pub fn generate_from_file(input: TokenStream) -> TokenStream {
+    let path = parse_macro_input!(input as LitStr).value();
+    generate::from_file(path.as_str())
+}
+
+/// Generate code from a string
+///
+#[proc_macro]
+pub fn generate_from_template(input: TokenStream) -> TokenStream {
+    let template = parse_macro_input!(input as LitStr).value();
+    generate::from_template(template.as_str())
 }
