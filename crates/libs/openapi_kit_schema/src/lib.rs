4b981637f8d1e46dd004b142df042074f2c5b792
wilberg <wilberg@netrune.com>
2025-03-02 19:20:07 +0100

    feat(schema): accept AsRef<Path> instead of &str in load function and relocate Error struct to own file

--- crates/libs/openapi_kit_schema/src/lib.rs
+++ crates/libs/openapi_kit_schema/src/lib.rs
@@ -1,11 +1,12 @@
+mod error;
+
+use std::path::Path;
+
+pub use error::Error;
+
 pub type OpenApiSchema = openapiv3::OpenAPI;
 
-pub enum Error {
-    Io(std::io::Error),
-    Serde(serde_yaml::Error),
-}
-
-pub fn load(path: &str) -> Result<OpenApiSchema, Error> {
+pub fn load<P: AsRef<Path>>(path: P) -> Result<OpenApiSchema, Error> {
     let file = std::fs::read_to_string(path).map_err(|e| Error::Io(e))?;
     serde_yaml::from_str(&file).map_err(|e| Error::Serde(e))
 }
