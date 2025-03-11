d59da2682c4ef4436d53736372a768544185d780
wilberg <wilberg@netrune.com>
2025-03-11 17:55:04 +0100

    feat(schema): shorten redundant map_err

--- crates/libs/openapi_kit_schema/src/lib.rs
+++ crates/libs/openapi_kit_schema/src/lib.rs
@@ -1,4 +1,5 @@
 mod error;
+mod resolve;
 
 use std::path::Path;
 
@@ -7,6 +8,6 @@ pub use error::Error;
 pub type OpenApiSchema = openapiv3::OpenAPI;
 
 pub fn load<P: AsRef<Path>>(path: P) -> Result<OpenApiSchema, Error> {
-    let file = std::fs::read_to_string(path).map_err(|e| Error::Io(e))?;
-    serde_yaml::from_str(&file).map_err(|e| Error::Serde(e))
+    let file = std::fs::read_to_string(path).map_err(Error::Io)?;
+    serde_yaml::from_str(&file).map_err(Error::Serde)
 }
